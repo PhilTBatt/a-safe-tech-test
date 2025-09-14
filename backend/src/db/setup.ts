f54fda59b27b5b28805a4c1b60727afeadf5515b
PhilTBatt <philtbatt@gmail.com>
2025-09-14 19:05:11 +0100

    fetch jokes to seed db

--- backend/src/db/setup.ts
+++ backend/src/db/setup.ts
@@ -1,6 +1,9 @@
 import db from './connection';
+import fetch from 'node-fetch';
 
 async function seed() {
+    const res = await fetch('https://raw.githubusercontent.com/15Dkatz/official_joke_api/master/jokes/index.json')
+    const jokes = await res.json()
 
     await db.query(`
         CREATE TABLE IF NOT EXISTS jokes (
@@ -11,7 +14,15 @@ async function seed() {
         )
     `)
 
+    for (const joke of jokes) {
+        await db.query(
+            'INSERT INTO jokes (type, setup, punchline) VALUES ($1, $2, $3)',
+            [joke.type, joke.setup, joke.punchline]
+        )
+    }
+
     await db.end()
+    console.log('Database seeded')
 }
 
 seed()
